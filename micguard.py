1d37806674f4e7d7763f6d70a62c1b32248934da
Bristopher <Bristopher@users.noreply.github.com>
2026-07-12 14:54:34 -0700

    Release v1.1.0

--- micguard.py
+++ micguard.py
@@ -20,7 +20,7 @@ import webbrowser
 import winreg
 
 APP_NAME = "MicGuard"
-VERSION = "1.0.0"
+VERSION = "1.1.0"
 GITHUB_REPO = "Bristopher/MicGuard"
 RELEASES_URL = f"https://github.com/{GITHUB_REPO}/releases/latest"
 CONFIG_DIR = os.path.join(os.environ["APPDATA"], APP_NAME)
